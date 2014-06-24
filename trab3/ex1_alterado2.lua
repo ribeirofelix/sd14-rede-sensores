83ab4044f42b7a809beb60a9c40f6a698bc0300e
TerraNet <terra@terra-VirtualBox.(none)>
2014-06-23 21:54:46 -0300

    colocando os arquivos da erica na pasta /trab3

--- trab3/ex1_alterado2.lua
+++ trab3/ex1_alterado2.lua
@@ -2,20 +2,9 @@ local tossam = require("tossam")
 
 local exit = false
 
-function getAllTemperature(timeout)
-	exit = false
+local newRequestNumber = 12
 
-
-	if not timeout or type(timeout)~="number" then
-		timeout = 15
-	end
-
-	while not(exit) do
-		local mote = tossam.connect("sf@localhost:9002",1)
-	--	local micaz = tossam.connect("serial@/dev/ttyUSB1:micaz",1)
-		if not(mote) then print("Connection error!"); return(1); end
-
-		mote:register [[ 
+local msgRegister = [[ 
 		  nx_struct msg_serial [151] { 
 			nx_uint8_t id;
 			nx_uint16_t source;
@@ -26,19 +15,44 @@ function getAllTemperature(timeout)
 		  }; 
 		]]
 
-		msg2 = {
-			id		= 9,
+function printMessage (msg)
+	print("------------------------------") 
+	print("msgID: "..msg.id, "Source: ".. msg.source, "Target: ".. msg.target) 
+	print("d8:",unpack(msg.d8))
+	print("d16:",unpack(msg.d16))
+	print("d32:",unpack(msg.d32))
+end
+
+function createNewRequestMessage ()
+	local msg = {
+			id		= newRequestNumber,
 			source	= 0,
 			target	= 1,
 			d8		= {2, 0, 0, 0},
 			d16		= {0, 0, 0, 0},
 			d32		= {0, 0}
 			}
+	newRequestNumber = newRequestNumber + 1
+	return msg
+end
 
+function getAllTemperature(timeout)
+	exit = false
+
+	if not timeout or type(timeout)~="number" then
+		timeout = 15
+	end
+
+	while not(exit) do
+		local mote = tossam.connect("sf@localhost:9002",1)
+	--	local micaz = tossam.connect("serial@/dev/ttyUSB1:micaz",1)
+		if not(mote) then print("Connection error!"); return(1); end
+
+		mote:register(msgRegister)
+		local msg2 = createNewRequestMessage()
 		mote:send(msg2,151,1)
 
 		local startTime = os.time()
-
 		while (mote) do
 			--
 			if (os.time()-startTime>timeout) then
@@ -52,11 +66,7 @@ function getAllTemperature(timeout)
 			if stat then
 				if msg then
 					if (msg.source ~= 0 and msg.d8[1] == 1) then
-						print("------------------------------") 
-						print("msgID: "..msg.id, "Source: ".. msg.source, "Target: ".. msg.target) 
-						print("d8:",unpack(msg.d8))
-						print("d16:",unpack(msg.d16))
-						print("d32:",unpack(msg.d32))
+						printMessage(msg)
 					else
 						--print "False"
 					end
--- trab3/stubs.lua
+++ trab3/stubs.lua
@@ -1,15 +1,17 @@
 local tossam = require("tossam") 
+
 local exit = false
+
 local msgId = nil
-requestMessage = {
-		id		= 1,
-		source	= 0,
-		target	= 1,
-		d8		= {0, 0, 0, 0},
-		d16		= {0, 0, 0, 0},
-		d32		= {0, 0}
-	}
-local messageStructure = [[ 
+
+function printMessage (msg)
+
+end
+
+function collectTemperatures ()
+	local mote = tossam.connect("sf@localhost:9002",1)
+	if not(mote) then print("Connection error!"); return(1); end
+	mote:register [[ 
 	  nx_struct msg_serial [151] { 
 		nx_uint8_t id;
 		nx_uint16_t source;
@@ -20,46 +22,28 @@ local messageStructure = [[
 	  }; 
 	]]
 
-function copyMessage(msg)
-	local newMsg = {}
-	newMsg.id = msg.id
-	newMsg.source = msg.source
-	newMsg.target = msg.target
-	newMsg.d8 = {}
-	newMsg.d16 = {}
-	newMsg.d32 = {}
-	for i = 1, 4 do
-		newMsg.d8[i] = msg.d8[i]
-		newMsg.d16[i] = msg.d16[i]
-		newMsg.d32[i] = msg.d32[i]
-	end
-	return newMsg
-end
+	msg2 = {
+		id		= 1,
+		source	= 0,
+		target	= 1,
+		d8		= {0, 0, 0, 0},
+		d16		= {0, 0, 0, 0},
+		d32		= {0, 0}
+	}
 
-function printMessage (msg)
-	print("------------------------------") 
-	print("msgID: "..msg.id, "Source: ".. msg.source, "Target: ".. msg.target) 
-	print("d8:",unpack(msg.d8))
-	print("d16:",unpack(msg.d16))
-	print("d32:",unpack(msg.d32))
-end
-
-function collectTemperatures ()
-	local mote = tossam.connect("sf@localhost:9002",1)
-	if not(mote) then print("Connection error!"); return(1); end
-	mote:register(messageStructure)
-	msg2 = copyMessage(requestMessage)
 	mote:send(msg2,151,1)
 
 	while (mote) do
-		print "mote"
 
 		local stat, msg, emsg = pcall(function() return mote:receive() end) 
 		if stat then
-			
 			if msg then
-				printMessage(msg)
-				msg.d8[1] = 2; -- para que os nos da rede ignorem essa mensagem;
+				print("------------------------------") 
+				print("msgID: "..msg.id, "Source: ".. msg.source, "Target: ".. msg.target) 
+				print("d8:",unpack(msg.d8))
+				print("d16:",unpack(msg.d16))
+				print("d32:",unpack(msg.d32))
+
 				msg.source = 0
 				msg.target = 1
 				mote:send(msg, 151,1)
@@ -68,9 +52,6 @@ function collectTemperatures ()
 					print("\nConnection closed!")
 					exit = true
 					break 
-				elseif emsg =="timeout" then
-					print "Time out."
-					
 				end
 			end
 		else
